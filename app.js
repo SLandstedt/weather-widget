8aa7ddc031de105ce343a0100d9339f2e5729150
Simon <simon.landstedt@gmail.com>
2019-06-11 13:41:59 +0200

    updated img and cleaned up code

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
-//var bannerElement = $('.main-banner-1');
-
 var bannerElement = $('.main-banner-1 .ratio-wrapper');
 
 
-
-
-
-
-//var aElement = bannerElement.children('a');
-
-
-bannerElement.empty().html('<img src="http://10.217.51.160:8887img/rain.png"/>');
+bannerElement.empty().html('<img src="https://slandstedt.github.io/weather-widget/img/rain.png"/>');
