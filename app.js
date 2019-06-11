9e9d327921bbb8afaf57beb598aa275f84d59cee
Simon <simon.landstedt@gmail.com>
2019-06-11 13:33:34 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//var bannerElement = $('.main-banner-1');
+
+var bannerElement = $('.main-banner-1 .ratio-wrapper');
+
+
+
+
+
+
+//var aElement = bannerElement.children('a');
+
+
+bannerElement.empty().html('<img src="http://10.217.51.160:8887img/rain.png"/>');
